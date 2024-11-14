2163841aef9bce038016d4c57777eb9b4e23b1c1
fadhluhakim <fadhlulhakim1@gmail.com>
2024-11-14 14:27:13 +0800

    fetch product list

--- src/components/shopPage/Pagination.tsx
+++ src/components/shopPage/Pagination.tsx
@@ -1,88 +1,125 @@
-import React, { useState } from "react";
-import ReactPaginate from "react-paginate";
-import Products from "../shopPage/Products";
-import { paginationItems } from "../../assets/frontend_assets/assets";
+import React, {useEffect, useState} from 'react';
+import ReactPaginate from 'react-paginate';
+import Products from '../shopPage/Products';
+// import {paginationItems} from '../../assets/frontend_assets/assets';
 
-const items = paginationItems;
+import {db} from '@/firebase/firebaseConfig';
+import {collection, getDocs} from 'firebase/firestore';
+import {assets} from '@/assets/frontend_assets/assets';
 
-interface Item {
-  _id: string;
-  img: string;
-  productName: string;
-  price: string;
-  color: string;
-  badge?: boolean; 
-  des: string;
+// const items = paginationItems;
+
+interface Product {
+    prodID: string;
+    prodName: string;
+    prodPrice: number;
+    p1: keyof typeof assets;
+    badge?: boolean;
+    desc: string;
 }
 
 interface ItemsProps {
-  currentItems: Item[];
+    currentItems: Product[];
 }
 
-function Items({ currentItems }: ItemsProps) {
-  return (
-    <>
-      {currentItems &&
-        currentItems.map((item) => (
-          <div key={item._id} className="w-full">
-            <Products
-              _id={item._id}
-              img={item.img}
-              productName={item.productName}
-              price={item.price}
-              color={item.color}
-              badge={item.badge}
-              des={item.des}
-            />
-          </div>
-        ))}
-    </>
-  );
+// interface Item {
+//     _id: string;
+//     img: string;
+//     productName: string;
+//     price: string;
+//     color: string;
+//     badge?: boolean;
+//     des: string;
+// }
+
+// interface ItemsProps {
+//     currentItems: Item[];
+// }
+
+function Items({currentItems}: ItemsProps) {
+    return (
+        <>
+            {currentItems &&
+                currentItems.map((item) => (
+                    <div key={item.prodID} className='w-full'>
+                        <Products
+                            _id={item.prodID}
+                            img={item.p1}
+                            productName={item.prodName}
+                            price={item.prodPrice.toString()}
+                            // color={item.color}
+                            badge={item.badge}
+                            des={item.desc}
+                        />
+                    </div>
+                ))}
+        </>
+    );
 }
 
 interface PaginationProps {
-  itemsPerPage: number;
+    itemsPerPage: number;
 }
 
-const Pagination: React.FC<PaginationProps> = ({ itemsPerPage }) => {
-  const [itemOffset, setItemOffset] = useState<number>(0);
-  const [itemStart, setItemStart] = useState<number>(1);
+const Pagination: React.FC<PaginationProps> = ({itemsPerPage}) => {
+    const [products, setProducts] = useState<Product[]>([]);
+    const [itemOffset, setItemOffset] = useState<number>(0);
+    const [itemStart, setItemStart] = useState<number>(1);
 
-  const endOffset = itemOffset + itemsPerPage;
-  const currentItems = items.slice(itemOffset, endOffset);
-  const pageCount = Math.ceil(items.length / itemsPerPage);
+    // Fetch products from Firestore when the component mounts
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const productCollectionRef = collection(db, 'Products');
+                const productSnapshot = await getDocs(productCollectionRef);
+                const productList = productSnapshot.docs.map(
+                    (doc) => doc.data() as Product,
+                );
+                setProducts(productList);
+            } catch (error) {
+                console.error('Error fetching products: ', error);
+            }
+        };
 
-  const handlePageClick = (event: { selected: number }) => {
-    const newOffset = (event.selected * itemsPerPage) % items.length;
-    setItemOffset(newOffset);
-    setItemStart(newOffset + 1); // Adjusted to show correct item start
-  };
+        fetchProducts();
+    }, []);
 
-  return (
-    <div>
-      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-10 mdl:gap-4 lg:gap-10">
-        <Items currentItems={currentItems} />
-      </div>
-      <div className="flex flex-col mdl:flex-row justify-center mdl:justify-between items-center">
-        <ReactPaginate
-          nextLabel=""
-          onPageChange={handlePageClick}
-          pageRangeDisplayed={3}
-          marginPagesDisplayed={2}
-          pageCount={pageCount}
-          previousLabel=""
-          pageLinkClassName="w-9 h-9 border-[1px] border-lightColor hover:border-tomato duration-300 flex justify-center items-center"
-          pageClassName="mr-6"
-          containerClassName="flex text-base font-semibold font-titleFont py-10"
-          activeClassName="bg-tomato text-white"
-        />
+    const endOffset = itemOffset + itemsPerPage;
+    const currentItems = products.slice(itemOffset, endOffset);
+    const pageCount = Math.ceil(products.length / itemsPerPage);
 
-        <p className="text-base font-normal text-lightText">
-          Products from {itemStart} to {endOffset} of {items.length}
-        </p>
-      </div>
-    </div>
-  );
+    const handlePageClick = (event: {selected: number}) => {
+        const newOffset = (event.selected * itemsPerPage) % products.length;
+        setItemOffset(newOffset);
+        setItemStart(newOffset + 1); // Adjusted to show correct item start
+    };
+
+    return (
+        <div>
+            <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-10 mdl:gap-4 lg:gap-10'>
+                <Items currentItems={currentItems} />
+            </div>
+            <div className='flex flex-col mdl:flex-row justify-center mdl:justify-between items-center'>
+                <ReactPaginate
+                    nextLabel=''
+                    onPageChange={handlePageClick}
+                    pageRangeDisplayed={3}
+                    marginPagesDisplayed={2}
+                    pageCount={pageCount}
+                    previousLabel=''
+                    pageLinkClassName='w-9 h-9 border-[1px] border-lightColor hover:border-tomato duration-300 flex justify-center items-center'
+                    pageClassName='mr-6'
+                    containerClassName='flex text-base font-semibold font-titleFont py-10'
+                    activeClassName='bg-tomato text-white'
+                />
+
+                <p className='text-base font-normal text-lightText'>
+                    Products from {itemStart} to {endOffset} of{' '}
+                    {products.length}
+                </p>
+            </div>
+        </div>
+    );
 };
 
 export default Pagination;
--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import {initializeApp} from 'firebase/app';
 import {getAuth} from 'firebase/auth';
+import {getFirestore} from 'firebase/firestore';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,7 +21,9 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-//auth
+// Initialize Firebase Auth
 const auth = getAuth(app);
+// Initialize Firestore
+const db = getFirestore(app);
 
-export {app, auth};
+export {app, auth, db};
